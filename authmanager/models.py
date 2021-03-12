0e941c0e31d05ac7c3629fb082004177db4234f7
Filip Mularski <mularskif@gmail.com>
2021-03-12 20:44:05 +0100

    postgresql setup, ExtendedUser model

---
+++ authmanager/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+class ExtendedUser(AbstractUser):
+    pin = models.CharField(max_length=64, null=False, default='0000')
+
--- passwordkeeper/settings.py
+++ passwordkeeper/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'authmanager.apps.AuthmanagerConfig'
 ]
 
 MIDDLEWARE = [
@@ -75,8 +77,10 @@ WSGI_APPLICATION = 'passwordkeeper.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('DB_NAME', ''),
+        'USER': os.environ.get('USER', ''),
+        'PASSWORD': os.environ.get('PASSWORD', '')
     }
 }
 
@@ -118,3 +122,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+AUTH_USER_MODEL = 'authmanager.ExtendedUser'
