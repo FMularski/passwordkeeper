5cfef1cc27e7b016b0c36bd5e1ba762296c65252
Filip Mularski <mularskif@gmail.com>
2021-03-15 00:05:36 +0100

    account form custom validation

--- home/forms.py
+++ home/forms.py
@@ -3,6 +3,22 @@ from .models import Account
 
 
 class AccountForm(forms.ModelForm):
+    def clean(self):
+        cleaned_data = super().clean()
+        login = cleaned_data.get('login')
+        email = cleaned_data.get('email')
+
+        if not (login or email):
+            raise forms.ValidationError('Either login or email is required.')
+
+        password = cleaned_data.get('password')
+        confirm = cleaned_data.get('confirm')
+
+        if password != confirm:
+            raise forms.ValidationError('Passwords don\'t match.')
+
+        return cleaned_data
+
     confirm = forms.CharField(max_length=256, widget=forms.PasswordInput(attrs={'class': 'nes-input'}))
     class Meta:
         model = Account
--- home/models.py
+++ home/models.py
@@ -3,8 +3,8 @@ from authmanager.models import ExtendedUser
 
 
 class Account(models.Model):
-    title = models.CharField(max_length=32, null=False)
-    login = models.CharField(max_length=64, null=False)
-    email = models.EmailField(max_length=64, null=False)
-    password = models.CharField(max_length=256, null=False)
+    title = models.CharField(max_length=32)
+    login = models.CharField(max_length=64, null=True, blank=True)
+    email = models.EmailField(max_length=64, null=True, blank=True)
+    password = models.CharField(max_length=256)
     owner = models.ForeignKey(ExtendedUser, on_delete=models.CASCADE, null=True)
