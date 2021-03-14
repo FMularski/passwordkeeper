8bf1fc90135851c3cc9b07e4beafedff8fe02c19
Filip Mularski <mularskif@gmail.com>
2021-03-14 12:13:58 +0100

    added owner field to account model

--- home/forms.py
+++ home/forms.py
@@ -5,7 +5,7 @@ from .models import Account
 class AccountForm(forms.ModelForm):
     class Meta:
         model = Account
-        fields = '__all__'
+        exclude = ['owner']
         widgets = {
             'title': forms.TextInput(attrs={'class': 'nes-input'}),
             'login': forms.TextInput(attrs={'class': 'nes-input'}),
--- home/models.py
+++ home/models.py
@@ -1,7 +1,10 @@
 from django.db import models
+from authmanager.models import ExtendedUser
+
 
 class Account(models.Model):
     title = models.CharField(max_length=32, null=False)
     login = models.CharField(max_length=64, null=False)
     email = models.EmailField(max_length=64, null=False)
     password = models.CharField(max_length=256, null=False)
+    owner = models.ForeignKey(ExtendedUser, on_delete=models.CASCADE, null=True)
