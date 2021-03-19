c12e436d424e7811dfc2dc2ba84c8a32769d58a7
Filip Mularski <mularskif@gmail.com>
2021-03-20 00:35:18 +0100

    encode_password and decode_password view helpers

--- view_helpers/home.py
+++ view_helpers/home.py
@@ -1,2 +1,24 @@
 def encode_password(password, pin):
-    return '*' * len(password)
+    pin_sum = 0
+    for char in pin:
+        pin_sum += ord(char)
+
+    encoded = ''
+
+    for char in password:
+        encoded += chr(ord(char) + pin_sum)
+
+    return encoded
+
+
+def decode_password(encoded, pin):
+    pin_sum = 0
+    for char in pin:
+        pin_sum += ord(char)
+
+    decoded = ''
+
+    for char in encoded:
+        decoded += chr(ord(char) - pin_sum)
+
+    return decoded
