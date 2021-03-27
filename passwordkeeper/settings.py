73c249df4aab4af60b42595d7f90ba8575f34956
Filip Mularski <mularskif@gmail.com>
2021-03-27 21:52:45 +0100

    secret key to env var

--- passwordkeeper/settings.py
+++ passwordkeeper/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'p58bee2$7(0wm76g9@63q^fz9rxz9$chg6(fpt$s7u7v53jp3y'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
