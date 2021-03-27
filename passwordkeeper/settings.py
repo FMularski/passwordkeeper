6178ad2759071e0c5667305266e56ccfad93ade6
Filip Mularski <mularskif@gmail.com>
2021-03-27 23:15:29 +0100

    add db host

--- passwordkeeper/settings.py
+++ passwordkeeper/settings.py
@@ -83,7 +83,8 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.environ.get('DB_NAME', ''),
         'USER': os.environ.get('USER', ''),
-        'PASSWORD': os.environ.get('PASSWORD', '')
+        'PASSWORD': os.environ.get('PASSWORD', ''),
+        'HOST': os.environ.get('HOST')
     }
 }
 
