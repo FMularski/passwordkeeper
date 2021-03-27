eccea80be0e22de00533f0381101df8ccbb4ef05
Filip Mularski <mularskif@gmail.com>
2021-03-27 21:41:54 +0100

    prepare for deploy

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn passwordkeeper.wsgi --log-file -
--- passwordkeeper/settings.py
+++ passwordkeeper/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'p58bee2$7(0wm76g9@63q^fz9rxz9$chg6(fpt$s7u7v53jp3y'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://app-password-keeper.herokuapp.com/', '127.0.0.1']
 
 
 # Application definition
@@ -51,6 +51,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware'
 ]
 
 ROOT_URLCONF = 'passwordkeeper.urls'
@@ -123,6 +124,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
